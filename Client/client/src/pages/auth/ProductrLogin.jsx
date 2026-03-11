ef996343469d663da901fc982cc38270fef863c5
unknown <jagdishmali32081@gmail.com>
2026-03-11 19:39:19 +0530

    updated code.

--- Client/client/src/pages/auth/ProductrLogin.jsx
+++ Client/client/src/pages/auth/ProductrLogin.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import bgImage from "../../assets/bg-Img.png";
 import logoIcon from "../../assets/logo-icon.png";
-import card from "../../assets/card.png";
+import card from "../../assets/card.jpg";
 
 import './Home.css';
 
